3ea64d9b0370227623db120217d188dff9a33e27
Dean Wake <deanmwake@gmail.com>
2021-07-19 22:57:13 +0200

    remove icons and classes

--- src/index.js
+++ src/index.js
@@ -7,13 +7,6 @@ function component() {
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
   
     return element;
   }
